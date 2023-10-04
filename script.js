862eb808a884d536e1aee76722d808569efbb5cd
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-05 01:16:46 +0600

    Modal open done with reflectoring. .

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 'use strict';
-const modal=document.querySelector('.modal');
-const overlay= document.querySelector('.overlay');
-const btnCloseModal=document.querySelector('.close-modal');
-const btnOpenModal=document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
 // console.log(btnCloseModal);
 // console.log(btnOpenModal);
 
-for(let i=0; i<btnOpenModal.length; i++){
-    btnOpenModal[i].addEventListener('click',()=>{
-        // its use to change js class
-      modal.classList.remove('hidden');
-      overlay.classList.remove('hidden');
-      
-    //   same, its use to change css style
-    //   modal.style.display='block';
-    //   overlay.style.display='block';
-    });
+const closeModal = () => {
+  // its use to change js class
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+
+  //   same, its use to change css style
+  //   modal.style.display='block';
+  //   overlay.style.display='block';
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', () => {
     // console.log(btnOpenModal[i].textContent);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
 
-btnCloseModal.addEventListener('click', ()=>{
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
